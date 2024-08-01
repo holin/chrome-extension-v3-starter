b92b33437f09ed5bc4498fb0547488658ac6c135
holin he <holin.he@gmail.com>
2024-08-01 12:43:24 +0800

    Works

--- foreground.js
+++ foreground.js
@@ -1,7 +1,66 @@
-// This script gets injected into any opened page
-// whose URL matches the pattern defined in the manifest
-// (see "content_script" key).
-// Several foreground scripts can be declared
-// and injected into the same or different pages.
+// console.log('$("#post").serialize()', $("#post").serialize())
+// $('#save-post').click()
 
-console.log("This prints to the console of the page (injected only if the page url matched)")
+function setupPostMutiButton() {
+  $('#save-post').after('<button id="multi-post">Multi Post</button>')
+  setTimeout(() => {
+    $('#multi-post').click(() => {
+      $('#content-html').click()
+      setTimeout(() => {
+        chrome.runtime.sendMessage({action: "multi-post", formData: $("#post").serializeArray()}, (response) => {
+          // 3. Got an asynchronous response with the data from the service worker
+          console.log('received user data', response);
+        });
+      }, 100);
+
+      return false
+    })
+  }, 1);
+}
+setupPostMutiButton()
+
+function createHiddenField(field) {
+  const html = '<input type="hidden" name="' + field.name + '" value="' + field.value + '">'
+  $('#post').append(html)
+}
+
+function populateForm(formData) {
+  const justSetValueFields = ['post_title', 'content', '_thumbnail_id']
+  for (let i = 0; i < formData.length; i++) {
+    let field = formData[i]
+    if (justSetValueFields.includes(field.name)) {
+      let element = $(`[name=${field.name}]`)
+      element.val(field.value)
+      if ('post_title' === field.name) {
+        setTimeout(() => {
+          console.log('focus')
+          element.focus()
+          $('#titlewrap label').click()
+          setTimeout(() => {
+            element.blur()
+            console.log('blur')
+          }, 1000);
+        }, 1000);
+      }
+    } else if (field.name == 'tax_input[product_category][]') {
+      $('#in-product_category-' + field.value).prop('checked', true)
+    } else if (field.name.indexOf('acf[field_') != -1) {
+      createHiddenField(field)
+    }
+  }
+}
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.action === "new-post") {
+      console.log('new-post with form data', request.formData)
+      populateForm(request.formData)
+      console.log('$("#post").serializeArray()', $("#post").serializeArray())
+
+      // save draft
+      $('#save-post').click()
+    }
+    sendResponse({farewell: "goodbye"});
+  }
+)
+
--- service-worker.js
+++ service-worker.js
@@ -10,3 +10,25 @@ importScripts('service-worker-utils.js')
 // If you want to import a file that is deeper in the file hierarchy of your
 // extension, simply do `importScripts('path/to/file.js')`.
 // The path should be relative to the file `manifest.json`.
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    if (request.action === "multi-post") {
+      chrome.tabs.create({url: 'http://localhost/wp-admin/post-new.php?post_type=product', active: false}, function (tab) {
+        console.log('tab.id', tab.id);
+        setTimeout(() => {
+          chrome.tabs.sendMessage(tab.id, {action: "new-post", formData: request.formData}, (response) => {
+            console.log('received user data', response);
+          });
+        }, 8000);
+      });
+
+      sendResponse({farewell: "goodbye"});
+
+    }
+
+  }
+)
